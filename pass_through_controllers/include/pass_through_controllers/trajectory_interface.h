d201d70a03b918b40d339dd5c561c0ca9552f47e
Stefan Scherzinger <scherzin@fzi.de>
2021-06-01 22:17:48 +0200

    Reorder joint names for joint-based trajectories
    Rationale:
    MoveIt generates joint trajectories with alphabetic joint order.
    This fix takes that into consideration and reorders the joints and
    associated data fields in the joint trajectory message according to
    a specified joint order.
    
    These changes are best placed into the trajectory interface where a
    type-based distinction is easier possible than in
    PassThroughControllers.

--- pass_through_controllers/include/pass_through_controllers/trajectory_interface.h
+++ pass_through_controllers/include/pass_through_controllers/trajectory_interface.h
@@ -146,18 +146,12 @@ class TrajectoryHandle
      * This will mainly be used by PassThroughControllers to store their new
      * incoming trajectories in the robot hardware interface.
      *
+     * Note: The JointTrajectory type reorders the joints according to the given
+     * joint resource list.
+     *
      * @param command The new trajectory
      */
-    void setCommand(TrajectoryType command)
-    {
-      assert(cmd_);
-      *cmd_ = command;
-
-      if (cmd_callback_)
-      {
-        cmd_callback_(*cmd_);
-      }
-    }
+    void setCommand(TrajectoryType command);
 
     /**
      * @brief Read a trajectory from the command buffer
@@ -214,13 +208,108 @@ class TrajectoryHandle
      */
     static std::string getName() noexcept;
 
+    /**
+     * @brief Set the order of joint names for trajectory reordering.
+     *
+     * @param joint_names
+     */
+    void setJointNames(const std::vector<std::string>& joint_names) noexcept {joint_names_ = joint_names;}
+
+    /**
+     * @brief Get the joint names associated with this handle
+     *
+     * @return Joint names
+     */
+    std::vector<std::string> getJointNames() const {return joint_names_;}
+
   private:
     TrajectoryType* cmd_;
     FeedbackType* feedback_;
     std::function<void(const TrajectoryType&)> cmd_callback_;
     std::function<void()> cancel_callback_;;
+    std::vector<std::string> joint_names_;
 };
 
+
+// Full spezialization for JointTrajectory
+template<> inline
+void TrajectoryHandle<JointTrajectory, JointTrajectoryFeedback>::setCommand(JointTrajectory command)
+{
+  control_msgs::FollowJointTrajectoryGoal tmp;
+
+  // Respect joint order by computing the map between msg indices to expected indices.
+  // If msg is {A, C, D, B} and expected is {A, B, C, D}, the associated mapping vector is {0, 2, 3, 1}
+  auto msg = command.trajectory.joint_names;
+  auto expected = joint_names_;
+  std::vector<size_t> msg_joints(msg.size());
+  if (msg.size() != expected.size())
+  {
+    // msg must contain all joint names.
+    ROS_WARN("Not forwarding trajectory. It contains wrong number of joints");
+    return;
+  }
+  for (auto msg_it = msg.begin(); msg_it != msg.end(); ++msg_it)
+  {
+    auto expected_it = std::find(expected.begin(), expected.end(), *msg_it);
+    if (expected.end() == expected_it)
+    {
+      ROS_WARN_STREAM("Not forwarding trajectory. It contains at least one unexpected joint name: " << *msg_it);
+      return;
+    }
+    else
+    {
+      const size_t msg_dist = std::distance(msg.begin(), msg_it);
+      const size_t expected_dist = std::distance(expected.begin(), expected_it);
+      msg_joints[msg_dist] = expected_dist;
+    }
+  }
+
+  // Reorder the joint names and data fields in all trajectory points
+  tmp.trajectory.joint_names = expected;
+
+  for (auto point : command.trajectory.points)
+  {
+    trajectory_msgs::JointTrajectoryPoint p{point};  // init for equal data size
+
+    for (size_t i = 0; i < expected.size(); ++i)
+    {
+      auto jnt_id = msg_joints[i];
+
+      if (point.positions.size() == expected.size())
+        p.positions[jnt_id] = point.positions[i];
+      if (point.velocities.size() == expected.size())
+        p.velocities[jnt_id] = point.velocities[i];
+      if (point.accelerations.size() == expected.size())
+        p.accelerations[jnt_id] = point.accelerations[i];
+      if (point.effort.size() == expected.size())
+        p.effort[jnt_id] = point.effort[i];
+    }
+    tmp.trajectory.points.push_back(p);
+  }
+
+  assert(cmd_);
+  *cmd_ = tmp;
+
+  if (cmd_callback_)
+  {
+    cmd_callback_(*cmd_);
+  }
+
+}
+
+
+// Full spezialization for CartesianTrajectory
+template<> inline
+void TrajectoryHandle<CartesianTrajectory, CartesianTrajectoryFeedback>::setCommand(CartesianTrajectory command)
+{
+  assert(cmd_);
+  *cmd_ = command;
+
+  if (cmd_callback_)
+  {
+    cmd_callback_(*cmd_);
+  }
+}
 
 // Full spezializations for name deduction
 template<> inline
@@ -260,7 +349,7 @@ class TrajectoryInterface
     /**
      * @brief Associate resources with this interface
      *
-     * Call this during initialization of your controller.
+     * Call this right after initialization.
      * \Note: Proper resource handling depends on calling this
      * method \a before acquiring handles to this interface via getHandle().
      *
@@ -289,8 +378,42 @@ class TrajectoryInterface
       }
     }
 
+    /**
+     * @brief Specialized override for joint trajectory handles
+     *
+     * This passes the resource names associated with this interface to the
+     * returned JointTrajectoryHandle, which can then make use of them for
+     * reordering the incoming joint trajectories.
+     *
+     * In the case of CartesianTrajectoryHandle, this forwards the call
+     * to \a getHandle from HardwareResourceManager.
+     *
+     * @param name Name of trajectory handle
+     *
+     * @return Trajectory handle associated to \e name.
+     */
+    TrajectoryHandle<TrajectoryType, FeedbackType> getHandle(const std::string& name);
+    
+
   private:
     std::vector<std::string> joint_names_;
+};
+
+template<> inline
+JointTrajectoryHandle TrajectoryInterface<JointTrajectory, JointTrajectoryFeedback>::getHandle(const std::string& name)
+{
+  JointTrajectoryHandle joint_handle = this->hardware_interface::HardwareResourceManager<
+    JointTrajectoryHandle, hardware_interface::ClaimResources>::getHandle(name);
+
+  joint_handle.setJointNames(joint_names_);
+  return joint_handle;
+};
+
+template<> inline
+CartesianTrajectoryHandle TrajectoryInterface<CartesianTrajectory, CartesianTrajectoryFeedback>::getHandle(const std::string& name)
+{
+  return this->hardware_interface::HardwareResourceManager<
+    CartesianTrajectoryHandle, hardware_interface::ClaimResources>::getHandle(name);
 };
 
 /**
